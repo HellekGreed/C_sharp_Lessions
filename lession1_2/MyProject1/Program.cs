d9435016389c0d17cb00903e56bc6e3775516491
Hellek-Greed <nimnarr@gmail.com>
2022-08-30 21:51:42 +0300

    MyProject1 commit added

---
+++ lession1_2/MyProject1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число чтобы узнать его квадрат: ");
+int number = int.Parse(Console.ReadLine());
+int result = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {result}");
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine($"Квадрат числа {number} равен {result_2}");
