0b67aed96876672ee2b817a9599e6a9a96af69c0
Hellek-Greed <nimnarr@gmail.com>
2022-08-30 21:51:56 +0300

    MyProject3 commit added

---
+++ lession1_2/MyProject3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+        Console.WriteLine("Понедельник");
+}
+if (day == 2)
+{
+        Console.WriteLine("Вторник");
+}
+if (day == 3)
+{
+        Console.WriteLine("Среда");
+}
+if (day == 4)
+{
+        Console.WriteLine("Четверг");
+}
+if (day == 5)
+{
+        Console.WriteLine("Пятница");
+}
+if (day == 6)
+{
+        Console.WriteLine("Суббота");
+}
+if (day == 7)
+{
+        Console.WriteLine("Воскресенье");
+}
