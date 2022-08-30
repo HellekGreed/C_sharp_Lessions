91bd69471a459dc234c29f4138ff42e1615744e2
Hellek-Greed <nimnarr@gmail.com>
2022-08-30 21:51:50 +0300

    MyProject2 commit added

---
+++ lession1_2/MyProject2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+if (number_1 == (number_2 * number_2))
+{
+        Console.WriteLine($"Число {number_1} является квадратом {number_2}");
+}
+else
+{
+        Console.WriteLine($"Число {number_1} не является квадратом {number_2}");
+}
