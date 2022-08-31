23fa281a0ce14d2a691aca0460fd02bd80c10449
Hellek-Greed <nimnarr@gmail.com>
2022-08-31 18:24:28 +0300

    MyProject4 fix

--- lession1_2/MyProject4/Program.cs
+++ lession1_2/MyProject4/Program.cs
@@ -1,10 +1,24 @@
 ﻿Console.Write("Введите целое число: ");
 int a = int.Parse(Console.ReadLine());
 int b = -a;
-
-while (b < a + 1)
+if (a > b)
 {
-        Console.Write($"{b} ");
-        b += 1;
+    while (b <= a)
+    {
+            Console.Write($"{b} ");
+            b++;
+    }
+}
+else if(b > a)
+{
+    while (a <= b)
+    {
+            Console.Write($"{a} ");
+            a++;
+    }
+}
+else if (a == 0)
+{
+        Console.Write(a);
 }
 
