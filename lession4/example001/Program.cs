59389de85ae8941860eb57e38227948c5691302c
Hellek-Greed <nimnarr@gmail.com>
2022-09-09 21:58:15 +0300

    example001 added

---
+++ lession4/example001/Program.cs
@@ -0,0 +1,19 @@
+﻿int Count(int number)
+{
+        tempNumber = Math.Abs(number);
+        int count = 0;
+        while(tempNumber > 0)
+        {
+                count++;
+                tempNumber /= 10;
+        }
+        return count;
+}
+
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = Count(num);
+Console.WriteLine(result);
+
+
