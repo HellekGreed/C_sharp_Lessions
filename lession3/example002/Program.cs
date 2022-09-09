0b40c4c59d429d532b0d0d4952145709ebf1e16f
Hellek-Greed <nimnarr@gmail.com>
2022-09-09 11:32:45 +0300

    Example002 added

---
+++ lession3/example002/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] arr = {1, 5 ,4, 3, 2 , 6, 7, 8, 1, 1};
+void PrintArray(int [] array)
+{
+        int count = array.Length;
+
+        for(int i = 0; i < count; i ++)
+        {
+                Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+}
+
+
+int[] SelectionSort(int[] array)
+{
+        for(int i = 0; i < array.Length - 1; i++)
+        {
+            int minPosition = i;
+
+            for(int j = i + 1; j < array.Length; j++)
+            {
+                if(array[minPosition] < array[j]) minPosition = j;
+            }
+
+            int temporary = array[i];
+            array[i] = array[minPosition];
+            array[minPosition] = temporary;
+
+        }
+
+        return array;
+}
+
+PrintArray(arr);
+arr = SelectionSort(arr);
+PrintArray(arr);
