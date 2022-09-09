e42f5efb68447b822c27096d10bb9b60cc7fd472
Hellek-Greed <nimnarr@gmail.com>
2022-09-09 21:58:21 +0300

    example002 added

---
+++ lession4/example002/Program.cs
@@ -0,0 +1,30 @@
+﻿int GetMult(int number)
+{
+    int mult = 1;
+    if(number > 0)
+
+        for(int i = 1; i <= number; i++)
+        {
+            mult *= i; // mult = mult * i
+        }
+
+    if(number < 0)
+
+    {
+        for(int i = -1; i >= number; i--)
+        {
+            mult *= i; // mult = mult * i
+        }
+
+    }
+
+
+    return mult;
+}
+
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = GetMult(num);
+Console.WriteLine(result);
+
