b6a4fbf4bd0c580b5383d02622e6e620b85aff95
Hellek-Greed <nimnarr@gmail.com>
2022-09-27 22:29:07 +0300

    Example 3 added

---
+++ lession9/example003/Program.cs
@@ -0,0 +1,16 @@
+﻿int Mult(int a, int b)
+{
+        int mul = 1;
+        if(b == 1)
+        {
+                return a;
+        }
+        return a * Mult(a, b -1);
+}
+
+int a = 2;
+int b = 3;
+
+int res = Mult(a, b);
+
+Console.Write(res);
