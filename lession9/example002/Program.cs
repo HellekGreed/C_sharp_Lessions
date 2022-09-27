7c245960d3526e86ea048f799eb3d8bcbeaf37ac
Hellek-Greed <nimnarr@gmail.com>
2022-09-27 22:28:59 +0300

    Example 2 added

---
+++ lession9/example002/Program.cs
@@ -0,0 +1,20 @@
+﻿int NumberSum(int n)
+{
+        int sum = 0;
+
+        if (n > 0)
+        {
+
+                sum = n % 10 + NumberSum(n/10);
+
+        }
+
+        return sum;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int res = NumberSum(n);
+
+Console.Write(res);
+
