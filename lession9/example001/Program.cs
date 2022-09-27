492076b28faa0e40d2b01217e52a50ffc2457b57
Hellek-Greed <nimnarr@gmail.com>
2022-09-27 22:28:51 +0300

    Example 1 added

---
+++ lession9/example001/Program.cs
@@ -0,0 +1,20 @@
+﻿int PrintNumAndSum(int n, int m)
+{
+        int sum = 0;
+        if(n == m)
+        {
+                Console.Write(1);
+                return m;
+        }
+        Console.Write($"{n} ");
+        sum = n + PrintNumAndSum(n - 1, m);
+        
+        return sum;
+}
+
+int n = 5;
+int m = 1;
+int sum = PrintNumAndSum(n, m);
+Console.WriteLine();
+Console.WriteLine(sum);
+
